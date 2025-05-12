18d48be0ee3e549808878ede0fb0034b770d7cc1
mirazola-ik <mirazola@ikerlan.es>
2025-05-12 16:49:56 +0200

    Update `offload_minio.py` example - Add more tests - Clean previous code - Add exception handling for error cases (within the current tests only)

--- examples/offload_minio.py
+++ examples/offload_minio.py
@@ -7,28 +7,31 @@ import json # optional
 
 # Execution requirements, dependencies and policies
 REQS_INIT = {
-      "FLAVOUR": "EnergyV2",
+      "FLAVOUR": "FaaS_generic_V2",
       "MIN_ENERGY_RENEWABLE_USAGE": 70,
       "GEOLOCATION": "IKERLAN ARRASATE/MONDRAGON 20500"
 }
 
 def minio_list_buckets():
     try:
-        # MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
-        MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL
-        MINIO_USERNAME = "minio_user"
-        MINIO_PASSWORD = "minio_psw"
+        MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
+        # MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL TESTS
         
-        from modules._minio_client import MinioClient # gets stuck in execution
+        from modules._logger import CognitLogger
+        cognit_logger = CognitLogger()
+        cognit_logger.debug(f" [DR] Listing buckets in {MINIO_ENDPOINT}")
+        
+        from modules._minio_client import MinioClient
         minio_client = MinioClient(
             endpoint_url=MINIO_ENDPOINT,
-            access_key=MINIO_USERNAME,
-            secret_key=MINIO_PASSWORD
+            access_key="minio_user",
+            secret_key="minio_psw"
         )
         response = minio_client.list_buckets()
         return response
     except Exception as e:
         return [f"Error listing buckets: {str(e)}"]
+    
 
 def create_bucket(bucket_name):
         """
@@ -37,20 +40,25 @@ def create_bucket(bucket_name):
         :param bucket_name: Name of the bucket to create.
         """
         try:
-            # MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
-            MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL
-            MINIO_USERNAME = "minio_user"
-            MINIO_PASSWORD = "minio_psw"
+            MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
+            # MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL TESTS
             
-            from modules._minio_client import MinioClient # gets stuck in execution
+            from modules._logger import CognitLogger
+            cognit_logger = CognitLogger()
+            cognit_logger.debug(f" [DR] Creating bucket {MINIO_ENDPOINT}/{bucket_name}")
+            
+            from modules._minio_client import MinioClient
             minio_client = MinioClient(
                 endpoint_url=MINIO_ENDPOINT,
-                access_key=MINIO_USERNAME,
-                secret_key=MINIO_PASSWORD
+                access_key="minio_user",
+                secret_key="minio_psw"
             )
             
             minio_client.create_bucket(bucket_name)
-            return f"Bucket '{bucket_name}' created successfully."
+            if bucket_name in minio_client.list_buckets():
+                return f"Bucket {bucket_name} created successfully"
+            else:
+                return f"No exceptions, but bucket {bucket_name} was not created"
         except Exception as e:
             return f"Failed to create bucket: {str(e)}"
 
@@ -64,16 +72,20 @@ def upload_data(bucket_name, minio_path, data, extraArgs=None):
     :param extraArgs: Extra arguments for the upload, such as metadata (optional).
     """
     try:
-        # MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
-        MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL
-        MINIO_USERNAME = "minio_user"
-        MINIO_PASSWORD = "minio_psw"
+        MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
+        # MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL TESTS
+        
+        
+        
+        from modules._logger import CognitLogger
+        cognit_logger = CognitLogger()
+        cognit_logger.debug(f" [DR] Uploading data to {MINIO_ENDPOINT}/{bucket_name}/{minio_path}")
         
         from modules._minio_client import MinioClient
         minio_client = MinioClient(
             endpoint_url=MINIO_ENDPOINT,
-            access_key=MINIO_USERNAME,
-            secret_key=MINIO_PASSWORD
+            access_key="minio_user",
+            secret_key="minio_psw"
         )
 
         minio_client.upload_object(
@@ -88,18 +100,19 @@ def upload_data(bucket_name, minio_path, data, extraArgs=None):
       
 
 def download_data_to_memory(bucket_name, minio_path):
-    # MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
-    MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL
-    MINIO_USERNAME = "minio_user"
-    MINIO_PASSWORD = "minio_psw"
+    MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
+    # MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL TESTS
+    
+    from modules._logger import CognitLogger
+    cognit_logger = CognitLogger()
+    cognit_logger.debug(f" [DR] Downloading data to memory from {MINIO_ENDPOINT}/{bucket_name}/{minio_path}")
     
     from modules._minio_client import MinioClient
     minio_client = MinioClient(
         endpoint_url=MINIO_ENDPOINT,
-        access_key=MINIO_USERNAME,
-        secret_key=MINIO_PASSWORD
+        access_key="minio_user",
+        secret_key="minio_psw"
     )
-    print("Downloading object from MinIO...")
     data = minio_client.download_object(
         bucket=bucket_name,
         key=minio_path,
@@ -107,17 +120,20 @@ def download_data_to_memory(bucket_name, minio_path):
     )
     return data
 
+
 def download_data_to_disk(bucket_name, minio_path, save_disk_path):
-    # MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
-    MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL
-    MINIO_USERNAME = "minio_user"
-    MINIO_PASSWORD = "minio_psw"
+    MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
+    # MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL TESTS
+    
+    from modules._logger import CognitLogger
+    cognit_logger = CognitLogger()
+    cognit_logger.debug(f" [DR] Downloading data to disk from {MINIO_ENDPOINT}/{bucket_name}/{minio_path}")
     
     from modules._minio_client import MinioClient
     minio_client = MinioClient(
         endpoint_url=MINIO_ENDPOINT,
-        access_key=MINIO_USERNAME,
-        secret_key=MINIO_PASSWORD
+        access_key="minio_user",
+        secret_key="minio_psw"
     )
     data = minio_client.download_object(
         bucket=bucket_name,
@@ -126,55 +142,137 @@ def download_data_to_disk(bucket_name, minio_path, save_disk_path):
     )
     return data
 
+def recursive_cleanup():
+    """
+    Recursivelly deletes all the objects and buckets in MinIO.
+    The bucket must be empty before deleting it, so first all the objects are deleted.
+    """
+    MINIO_ENDPOINT = "http://192.168.120.100:9000" # TESTBED
+    # MINIO_ENDPOINT = "http://127.0.0.1:9000" # LOCAL TESTS
+    
+    from modules._minio_client import MinioClient
+    minio_client = MinioClient(
+        endpoint_url=MINIO_ENDPOINT,
+        access_key="minio_user",
+        secret_key="minio_psw"
+    )
+    
+    ## Use this to enable logging mechanism for the offloaded function in the SR:
+    from modules._logger import CognitLogger
+    cognit_logger = CognitLogger()
+    ##
+    
+    bucket_list = minio_client.list_buckets()
+    for bucket in bucket_list:
+        cognit_logger.debug("######################################\n")
+        cognit_logger.debug("Deleting all objects in bucket: " + str(bucket))
+        objects = minio_client.list_objects(bucket)
+        for obj in objects:
+            cognit_logger.debug("Deleting object: " + str(obj))
+            minio_client.delete_object(bucket, obj)
+        cognit_logger.debug("Bucket is empty, deleting bucket: " + str(bucket))
+        minio_client.delete_bucket(bucket)
+        cognit_logger.debug("######################################\n")
+        
+    return len(minio_client.list_buckets()) == 0 # Returns True if all buckets have been deleted
 
 
-try:
-    # Instantiate a device Device Runtime
+def run_rest_large_files(my_device_runtime, bucket_name): # Currently (12/05/2025) not working
+    # 1. Upload to MinIO
+    minio_path = "test/test_large_file.json.gz"
+    with open("examples/2015-01-01-15.json.gz", "rb") as f:
+        json_data = f.read()
+    result = my_device_runtime.call(upload_data, bucket_name, minio_path, json_data)
+    print("Upload result: " + str(result.ret_code))
+    print("######################################\n")
+    # 2. Download from MinIO
+    result = my_device_runtime.call(download_data_to_memory, bucket_name, "test/test_large_file.json.gz")
+    print("Download result: " + str(result.ret_code))
+    print("######################################\n")
+
+
+def run_minio_example():
+    try:
+        # Instantiate a device Device Runtime
+        my_device_runtime = device_runtime.DeviceRuntime("./examples/cognit-template.yml")
+        my_device_runtime.init(REQS_INIT)
+        bucket_name = "ikerlan"
+        minio_path = "test/test.json" # minio will automatically create a folder named "test" in the bucket and create the file "test.json" inside
+             
+        # List buckets
+        # return_code, result = my_device_runtime.call(minio_list_buckets)
+        result = my_device_runtime.call(minio_list_buckets)
+        # print("Status code: " + str(return_code))
+        print("Available buckets: " + str(result.res))
+        print("######################################\n")
+        if (len(result.res) > 0) and ("ERROR" in result.res[0]): # error happened
+            print("Error listing buckets, exiting...")
+            return -1
+        
+        # If necessary, create the bucket:
+        if bucket_name not in result.res:
+            result = my_device_runtime.call(create_bucket, bucket_name)
+            print("Bucket creation result: " + str(result.res))
+            print("######################################\n")
+        
+        ## Upload data
+        simple_dict = {"a": 100, "b": 200, "testID":133}
+        # Convert dict to JSON and then to bytes
+        json_data = json.dumps(simple_dict).encode("utf-8")
+        result = my_device_runtime.call(upload_data, bucket_name, minio_path, json_data, {'ContentType': 'application/json'})
+        print("Upload result: " + str(result.res))
+        print("######################################\n")
+        
+        result = my_device_runtime.call(download_data_to_memory, bucket_name, minio_path)
+        data = result.res
+        if data is not None:
+            print("Data retrieved: " + str(data))
+            # text = data.decode('utf-8')        # for text files
+            obj  = json.loads(data)            # for JSON
+            print(obj)
+            # img  = Image.open(BytesIO(data))   # for images
+            print("######################################\n")
+        
+        
+        
+        ### Test with large file (3.7MB).
+        ### Previously downloaded to device running the DR using -> github-device-runtime-py/examples$ wget https://data.gharchive.org/2015-01-01-15.json.gz
+        if False: # Tested, but gives exception currently
+            try:
+                run_rest_large_files(my_device_runtime, bucket_name)
+                # It gives: HTTPError: 413 Client Error: Request Entity Too Large for url: https://194.28.122.87/v1/functions/20666/execute?app_req_id=20669&mode=sync
+            except Exception as e:
+                print("An exception has occured: " + str(e))
+                print("######################################\n")
+        
+        ### Example of download to SR disk (data remains in SR, not in DR)
+        if False: # Tested, but unusued currently
+            result = my_device_runtime.call(download_data_to_disk, bucket_name, minio_path, "/tmp/test.json")
+            data = result.res
+            if data is not None:
+                print(f"Download path is {str(data)}, verify in the SR ") 
+                print("######################################\n")
+        
+        ## Cleanup -> Delete all buckets and objects
+        print("Cleaning all buckets and objects...")
+        result = my_device_runtime.call(recursive_cleanup)
+        print("Cleanup result: " + str(result.res))
+        return 0
+
+    except Exception as e:
+        print("An exception has occured: " + str(e))
+        return -1
+
+
+def run_cleanup():
     my_device_runtime = device_runtime.DeviceRuntime("./examples/cognit-template.yml")
     my_device_runtime.init(REQS_INIT)
-    bucket_name = "ikerlan"
-    minio_path = "test/test.json" # minio will automatically create a folder named "test" in the bucket and create the file "test.json" inside
-    
-    # List buckets
-    # return_code, result = my_device_runtime.call(minio_list_buckets)
-    result = my_device_runtime.call(minio_list_buckets)
+    result = my_device_runtime.call(recursive_cleanup)
     # print("Status code: " + str(return_code))
-    print("Available buckets: " + str(result.res))
-    print("######################################\n")
+    print("Cleanup result: " + str(result.res))
     
-    # If necessary, create the bucket:
-    if bucket_name not in result.res:
-        result = my_device_runtime.call(create_bucket, bucket_name)
-        print("Bucket creation result: " + str(result.res))
-        print("######################################\n")
-    # Upload data
-    
-    simple_dict = {"a": 100, "b": 200, "testID":133}
-    # Convert dict to JSON and then to bytes
-    json_data = json.dumps(simple_dict).encode("utf-8")
-    result = my_device_runtime.call(upload_data, bucket_name, minio_path, json_data, {'ContentType': 'application/json'})
-    print("Upload result: " + str(result.res))
-    print("######################################\n")
-    
-    result = my_device_runtime.call(download_data_to_memory, bucket_name, minio_path)
-    data = result.res
-    if data is not None:
-        print("Data retrieved: " + str(data))
-        # text = data.decode('utf-8')        # for text files
-        obj  = json.loads(data)            # for JSON
-        print(obj)
-        # img  = Image.open(BytesIO(data))   # for images
-        print("######################################\n")
-    
-    # Example of download to disk
-    result = my_device_runtime.call(download_data_to_disk, bucket_name, minio_path, "/tmp/test.json")
-    data = result.res
-    if data is not None:
-        print("Download path: " + str(data))
-        print("######################################\n")
-    print("######################################\n")
 
-
-except Exception as e:
-    print("An exception has occured: " + str(e))
-    exit(-1)
+if __name__ == "__main__":
+    run_minio_example()
+    # run_cleanup() -> Called as last step inside 'run_minio_example' 
+    print("Exit OK")
