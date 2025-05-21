8e69e821497e0f8180567438a29ba2b323f078f1
Aritz Brosa <abrosa@ikerlan.es>
2025-05-21 10:02:10 +0200

    Remove calls to .stop in examples, as this was modified to be handled by a daemon thread on the state machine

--- examples/minimal_offload_sync.py
+++ examples/minimal_offload_sync.py
@@ -133,9 +133,6 @@ try:
     print("Multiply sync result: " + str(result))
     print("----------------------------------------------------")
 
-    # Stop device runtime
-    my_device_runtime.stop()
-
 except Exception as e:
     print("An exception has occured: " + str(e))
     exit(-1)
