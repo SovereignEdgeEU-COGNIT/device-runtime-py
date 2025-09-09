ee51618b41299a43e3df11c40dde8afa2b5713f4
Daniele Mingolla <dmingolla@opennebula.io>
2025-09-09 13:50:55 +0200

    Add demo script for testing scalability with Cognit Frontend

---
+++ examples/demo_cognit_code.py
@@ -0,0 +1,42 @@
+"""
+This script is used to test the scalability of the system when offloading functions to the Cognit Frontend.
+
+It is important to note that the COGNIT client is now sending the flavour as parameter in the request.
+In this way the NGINX server is able to route the request to the correct backend.
+"""
+
+import sys
+import time
+import os
+
+from cognit import device_runtime
+
+# Functions used to be uploaded
+def sum(a: int, b: int):
+    return a + b
+
+REQUIREMENTS = {
+      "FLAVOUR": "Nature",
+      "MAX_LATENCY": 30,
+      "GEOLOCATION": "59.3294,18.0687"
+}
+
+N_ITERATIONS = 200
+
+try:
+
+    rt = device_runtime.DeviceRuntime("./examples/cognit-template.yml")
+    rt.init(REQUIREMENTS)
+
+    for i in range(N_ITERATIONS):
+        print(f"Iteration {i}")
+        result = rt.call(sum, 17, 5)
+        print("-----------------------------------------------")
+        print("Flavour: " + REQUIREMENTS["FLAVOUR"] + ", Sum sync result: " + str(result))
+        print("-----------------------------------------------")
+    print("out of for...")
+    exit(0)
+
+except Exception as e:
+    print("An exception has occured: " + str(e))
+    exit(-1)
